82198d09c8934ca206482da9d16861b15c54ee26
NguyenHoangTien24012000 <hoangtienflytom@gmail.com>
2022-02-16 18:07:50 +0700

    update 11

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware,fixRequestBody } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware(['/adxType/*', '/adxType/*/*', '/api/*/*', '/adxItem/*/*', '/adxContact/*', '/imageAdx/*', '/admin/*/*/*', '/admin/*'], {
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+        }),
+    )
+};
